cffbe873136b4ae0c0f180d77a9897a02b8c11d4
Nathan de Graaf <nathan@brancom.nl>
2026-08-26 14:56:39 +0200

    Change bundle name

--- config/services.php
+++ config/services.php
@@ -63,7 +63,7 @@ return [
         'enabled' => env('APNS_ENABLED', false),
         'team_id' => env('APNS_TEAM_ID'),
         'key_id' => env('APNS_KEY_ID'),
-        'bundle_id' => env('APNS_BUNDLE_ID', 'nl.taskcheck.app'),
+        'bundle_id' => env('APNS_BUNDLE_ID', 'nl.brancom.taskcheck'),
         'private_key' => env('APNS_PRIVATE_KEY'),
         'private_key_path' => env('APNS_PRIVATE_KEY_PATH'),
         'production' => env('APNS_PRODUCTION', true),
